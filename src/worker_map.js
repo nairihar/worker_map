444a18d8645d07413f8529e86a7b0679de39da70
Nairi Harutyunyan <nairi.haroutiounian@gmail.com>
2023-09-27 13:39:39 +0300

    Add CI

--- src/worker_map.js
+++ src/worker_map.js
@@ -1,216 +1,217 @@
-const decoder = new TextDecoder('utf8');
-const encoder = new TextEncoder('utf8');
+const decoder = new TextDecoder("utf8");
+const encoder = new TextEncoder("utf8");
 
-const { lock, unlock, UNLCOKED } = require('./mutex');
+const { lock, unlock, UNLCOKED } = require("./mutex");
 
 const DEFAULT_OBJECT_BYTE_LENGTH = 4096; // total characters of stringified object (2^12)
 const MAX_OBJECT_BYTE_LENGTH = 4294967296; // max characters length for the stringified object (2^32)
 
-const MAP_SIZE = Symbol('MAP_SIZE');
-const PLAIN_OBJECT = Symbol('PLAIN_OBJECT');
-const VALUE_BUFFER = Symbol('VALUE_BUFFER');
-const SHARED_BUFFER = Symbol('SHARED_BUFFER');
+const MAP_SIZE = Symbol("MAP_SIZE");
+const PLAIN_OBJECT = Symbol("PLAIN_OBJECT");
+const VALUE_BUFFER = Symbol("VALUE_BUFFER");
+const SHARED_BUFFER = Symbol("SHARED_BUFFER");
 
 function getSharedMemoryBuffer(sharedBuffer) {
-    return new Int32Array(sharedBuffer);
+  return new Int32Array(sharedBuffer);
 }
 
 function calculateUsedBufferLength(valueBuffer) {
-    // exclude first item
-    return valueBuffer.reduce((acc, value) => (
-        acc + (value ? 1 : 0)
-    ), -1);
+  // exclude first item
+  return valueBuffer.reduce((acc, value) => acc + (value ? 1 : 0), -1);
 }
 
 function loadPlainSharedObject(valueBuffer) {
-    const usedBufferLength = calculateUsedBufferLength(valueBuffer);
-    const decodeBuffer = new Uint8Array(usedBufferLength);
+  const usedBufferLength = calculateUsedBufferLength(valueBuffer);
+  const decodeBuffer = new Uint8Array(usedBufferLength);
 
-    for (let i = 1; i <= usedBufferLength; i++) {
-        if (valueBuffer[i]) {
-            decodeBuffer[i - 1] = valueBuffer[i];
-        }
+  for (let i = 1; i <= usedBufferLength; i++) {
+    if (valueBuffer[i]) {
+      decodeBuffer[i - 1] = valueBuffer[i];
     }
+  }
 
-    return JSON.parse(
-        decoder.decode(decodeBuffer)
-    );
+  return JSON.parse(decoder.decode(decodeBuffer));
 }
 
 function safeLoadSharedObject(valueBuffer) {
-    lock(valueBuffer);
+  lock(valueBuffer);
 
-    const sharedObject = loadPlainSharedObject(valueBuffer)
+  const sharedObject = loadPlainSharedObject(valueBuffer);
 
-    unlock(valueBuffer);
+  unlock(valueBuffer);
 
-    return sharedObject;
+  return sharedObject;
 }
 
-
 function calculateStringByteLength(string) {
-    return encoder.encode(string).length;
+  return encoder.encode(string).length;
 }
 
 function saveObjectInBuffer(sharedObject, valueBuffer, objString) {
-    objString ??= JSON.stringify(sharedObject);
+  objString ??= JSON.stringify(sharedObject);
 
-    const stringByteLength = calculateStringByteLength(objString);
-    const encodeBuffer = new Uint8Array(stringByteLength);
-    encoder.encodeInto(objString, encodeBuffer)
+  const stringByteLength = calculateStringByteLength(objString);
+  const encodeBuffer = new Uint8Array(stringByteLength);
+  encoder.encodeInto(objString, encodeBuffer);
 
-    for (let i = 0; i < stringByteLength; i++) {
-        valueBuffer[i + 1] = encodeBuffer[i];
-    }
+  for (let i = 0; i < stringByteLength; i++) {
+    valueBuffer[i + 1] = encodeBuffer[i];
+  }
 
-    for (let j = stringByteLength + 1; j < valueBuffer.length; j++) {
-        valueBuffer[j] = 0;
-    }
+  for (let j = stringByteLength + 1; j < valueBuffer.length; j++) {
+    valueBuffer[j] = 0;
+  }
 }
 
 function adjustSharedBufferGrow(sharedBuffer, sharedObject) {
-    const objectByteLength = calculateStringByteLength(JSON.stringify(sharedObject));
-    const diff = (sharedBuffer.byteLength / 4) - objectByteLength;
+  const objectByteLength = calculateStringByteLength(
+    JSON.stringify(sharedObject),
+  );
+  const diff = sharedBuffer.byteLength / 4 - objectByteLength;
 
-
-    if (diff <= 0) {
-        if (!sharedBuffer.grow) {
-            throw new Error('No more space, create a new bigger shared object or use Node >= v20 to support auto grow.');
-        }
-
-        // byte length of Int32Array should be a multiple of 4
-        let growedLength = sharedBuffer.byteLength + (4 * Math.abs(diff)) + 64;
-
-        sharedBuffer.grow(growedLength);
+  if (diff <= 0) {
+    if (!sharedBuffer.grow) {
+      throw new Error(
+        "No more space, create a new bigger shared object or use Node >= v20 to support auto grow.",
+      );
     }
+
+    // byte length of Int32Array should be a multiple of 4
+    const growedLength = sharedBuffer.byteLength + 4 * Math.abs(diff) + 64;
+
+    sharedBuffer.grow(growedLength);
+  }
 }
 
-function WorkerMap(providedObject, length=DEFAULT_OBJECT_BYTE_LENGTH) {
-    if (providedObject instanceof SharedArrayBuffer) {
-        const sharedBuffer = providedObject;
+function WorkerMap(providedObject, length = DEFAULT_OBJECT_BYTE_LENGTH) {
+  if (providedObject instanceof SharedArrayBuffer) {
+    const sharedBuffer = providedObject;
 
-        const valueBuffer = getSharedMemoryBuffer(sharedBuffer);
-    
-        this[SHARED_BUFFER] = sharedBuffer;
-        this[VALUE_BUFFER] = valueBuffer;
-        this[MAP_SIZE] = this.keys().length;
-
-        return this;
-    }
-
-    const initialObject = providedObject || {};
-    const stringifiedObject = JSON.stringify(initialObject);
-
-    length += length % 4; // should be a multiple of 4
-
-    if (stringifiedObject.length >= length) {
-        length = 4 * stringifiedObject.length;
-    }
-
-    const sharedBuffer = new SharedArrayBuffer(Int32Array.BYTES_PER_ELEMENT * length, {
-        maxByteLength: MAX_OBJECT_BYTE_LENGTH,
-    });
     const valueBuffer = getSharedMemoryBuffer(sharedBuffer);
-
-    valueBuffer[0] = UNLCOKED; // first value used for locking and unlocking
-
-    saveObjectInBuffer(null, valueBuffer, stringifiedObject);
 
     this[SHARED_BUFFER] = sharedBuffer;
     this[VALUE_BUFFER] = valueBuffer;
-    this[MAP_SIZE] = 0;
+    this[MAP_SIZE] = this.keys().length;
 
     return this;
+  }
+
+  const initialObject = providedObject || {};
+  const stringifiedObject = JSON.stringify(initialObject);
+
+  length += length % 4; // should be a multiple of 4
+
+  if (stringifiedObject.length >= length) {
+    length = 4 * stringifiedObject.length;
+  }
+
+  const sharedBuffer = new SharedArrayBuffer(
+    Int32Array.BYTES_PER_ELEMENT * length,
+    {
+      maxByteLength: MAX_OBJECT_BYTE_LENGTH,
+    },
+  );
+  const valueBuffer = getSharedMemoryBuffer(sharedBuffer);
+
+  valueBuffer[0] = UNLCOKED; // first value used for locking and unlocking
+
+  saveObjectInBuffer(null, valueBuffer, stringifiedObject);
+
+  this[SHARED_BUFFER] = sharedBuffer;
+  this[VALUE_BUFFER] = valueBuffer;
+  this[MAP_SIZE] = 0;
+
+  return this;
 }
 
 WorkerMap.prototype.set = function (key, value) {
-    if (typeof key === 'function' || typeof value === 'function') {
-        return false;
-    }
+  if (typeof key === "function" || typeof value === "function") {
+    return false;
+  }
 
-    const valueBuffer = this[VALUE_BUFFER];
-    const sharedBuffer = this[SHARED_BUFFER];
+  const valueBuffer = this[VALUE_BUFFER];
+  const sharedBuffer = this[SHARED_BUFFER];
 
-    if (Number.isNaN(value) || value === undefined) {
-        value = null;
-    }
+  if (Number.isNaN(value) || value === undefined) {
+    value = null;
+  }
 
-    lock(valueBuffer);
+  lock(valueBuffer);
 
-    const sharedObject = loadPlainSharedObject(valueBuffer)
-    
-    sharedObject[key] = value;
+  const sharedObject = loadPlainSharedObject(valueBuffer);
 
-    adjustSharedBufferGrow(sharedBuffer, sharedObject);
+  sharedObject[key] = value;
 
-    saveObjectInBuffer(sharedObject, valueBuffer);
+  adjustSharedBufferGrow(sharedBuffer, sharedObject);
 
-    this[MAP_SIZE] = Object.keys(sharedObject).length;
+  saveObjectInBuffer(sharedObject, valueBuffer);
 
-    unlock(valueBuffer);
+  this[MAP_SIZE] = Object.keys(sharedObject).length;
 
-    return true;
+  unlock(valueBuffer);
+
+  return true;
 };
 
 WorkerMap.prototype.get = function (key) {
-    const valueBuffer = this[VALUE_BUFFER];
-    const sharedObject = safeLoadSharedObject(valueBuffer)
+  const valueBuffer = this[VALUE_BUFFER];
+  const sharedObject = safeLoadSharedObject(valueBuffer);
 
-    if (key === PLAIN_OBJECT) {
-        return sharedObject;
-    }
+  if (key === PLAIN_OBJECT) {
+    return sharedObject;
+  }
 
-    return sharedObject[key];
+  return sharedObject[key];
 };
 
 WorkerMap.prototype.delete = function (key) {
-    const valueBuffer = this[VALUE_BUFFER];
+  const valueBuffer = this[VALUE_BUFFER];
 
-    lock(valueBuffer);
+  lock(valueBuffer);
 
-    const sharedObject = loadPlainSharedObject(valueBuffer)
-    
-    if (sharedObject[key] === undefined) {
-        unlock(valueBuffer);
+  const sharedObject = loadPlainSharedObject(valueBuffer);
 
-        return false;
-    }
-    
-    delete sharedObject[key];
-
-    saveObjectInBuffer(sharedObject, valueBuffer);
-
+  if (sharedObject[key] === undefined) {
     unlock(valueBuffer);
 
-    this[MAP_SIZE] = Object.keys(sharedObject).length;
+    return false;
+  }
 
-    return true;
+  delete sharedObject[key];
+
+  saveObjectInBuffer(sharedObject, valueBuffer);
+
+  unlock(valueBuffer);
+
+  this[MAP_SIZE] = Object.keys(sharedObject).length;
+
+  return true;
 };
 
-WorkerMap.prototype.size = function (key) {
-    return this[MAP_SIZE];
+WorkerMap.prototype.size = function () {
+  return this[MAP_SIZE];
 };
 
 WorkerMap.prototype.keys = function () {
-    const valueBuffer = this[VALUE_BUFFER];
-    const sharedObject = safeLoadSharedObject(valueBuffer)
-    
-    return Object.keys(sharedObject);
+  const valueBuffer = this[VALUE_BUFFER];
+  const sharedObject = safeLoadSharedObject(valueBuffer);
+
+  return Object.keys(sharedObject);
 };
 
 WorkerMap.prototype.has = function (key) {
-    const valueBuffer = this[VALUE_BUFFER];
-    const sharedObject = safeLoadSharedObject(valueBuffer)
+  const valueBuffer = this[VALUE_BUFFER];
+  const sharedObject = safeLoadSharedObject(valueBuffer);
 
-    return sharedObject[key] !== undefined;
+  return sharedObject[key] !== undefined;
 };
 
 WorkerMap.prototype.toSharedBuffer = function () {
-    return this[SHARED_BUFFER];
+  return this[SHARED_BUFFER];
 };
 
-WorkerMap.prototype.toObject = function() {
-    return this.get(PLAIN_OBJECT);
+WorkerMap.prototype.toObject = function () {
+  return this.get(PLAIN_OBJECT);
 };
 
 module.exports = { WorkerMap };
--- test/worker_map.spec.js
+++ test/worker_map.spec.js
@@ -1,72 +1,71 @@
-const test = require('node:test');
-const assert = require('node:assert');
+const test = require("node:test");
+const assert = require("node:assert");
 
-const { WorkerMap } = require('../src/worker_map');
+const { WorkerMap } = require("../src/worker_map");
 
 let map = null;
 
-test('Create a map structure', () => {
-    map = new WorkerMap();
+test("Create a map structure", () => {
+  map = new WorkerMap();
 });
 
-test('.set()', () => {
-    map.set('balance', 100);
-    map.set('name', 'John');
-    map.set('nameArmenian', 'Ջոն');
-    map.set('lovesSport', true);
-    map.set('numbers', [ 1, 2, 3, 4]);
-    map.set('details', {
-        citizen: {
-            country: 'Germany',
-            passportYear: 1999,
-        }
-    });
+test(".set()", () => {
+  map.set("balance", 100);
+  map.set("name", "John");
+  map.set("nameArmenian", "Ջոն");
+  map.set("lovesSport", true);
+  map.set("numbers", [1, 2, 3, 4]);
+  map.set("details", {
+    citizen: {
+      country: "Germany",
+      passportYear: 1999,
+    },
+  });
 });
 
-test('.get()', () => {
-    assert.strictEqual(map.get('balance'), 100);
-    assert.strictEqual(map.get('name'), 'John');
-    assert.strictEqual(map.get('nameArmenian'), 'Ջոն');
-    assert.strictEqual(map.get('lovesSport'), true);
-    assert.strictEqual(map.get('numbers').length, 4);
-    assert.strictEqual(map.get('details').citizen.country, 'Germany');
+test(".get()", () => {
+  assert.strictEqual(map.get("balance"), 100);
+  assert.strictEqual(map.get("name"), "John");
+  assert.strictEqual(map.get("nameArmenian"), "Ջոն");
+  assert.strictEqual(map.get("lovesSport"), true);
+  assert.strictEqual(map.get("numbers").length, 4);
+  assert.strictEqual(map.get("details").citizen.country, "Germany");
 });
 
-test('.has', () => {
-    assert.strictEqual(map.has('balance'), true);
-    assert.strictEqual(map.has('name'), true);
-    assert.strictEqual(map.has('nameLatin'), false);
-    assert.strictEqual(map.has('text'), false);
-    assert.strictEqual(map.has('numbers'), true);
+test(".has", () => {
+  assert.strictEqual(map.has("balance"), true);
+  assert.strictEqual(map.has("name"), true);
+  assert.strictEqual(map.has("nameLatin"), false);
+  assert.strictEqual(map.has("text"), false);
+  assert.strictEqual(map.has("numbers"), true);
 });
 
-test('.delete', () => {
+test(".delete", () => {
+  assert.strictEqual(map.delete("balance"), true);
+  assert.strictEqual(map.has("balance"), false);
 
-    assert.strictEqual(map.delete('balance'), true);
-    assert.strictEqual(map.has('balance'), false);
-
-    assert.strictEqual(map.delete('account'), false);
+  assert.strictEqual(map.delete("account"), false);
 });
 
-test('.size', () => {
-    assert.strictEqual(map.size(), 5);
+test(".size", () => {
+  assert.strictEqual(map.size(), 5);
 });
 
-test('.keys', () => {
-    assert.strictEqual(map.keys().length, 5);
-    assert.strictEqual(map.keys()[0], 'name');
+test(".keys", () => {
+  assert.strictEqual(map.keys().length, 5);
+  assert.strictEqual(map.keys()[0], "name");
 });
 
-test('.toSharedBuffer', () => {
-    const sharedBuffer = map.toSharedBuffer();
-    const sameMap = new WorkerMap(sharedBuffer);
+test(".toSharedBuffer", () => {
+  const sharedBuffer = map.toSharedBuffer();
+  const sameMap = new WorkerMap(sharedBuffer);
 
-    assert.strictEqual(sameMap.size(), 5);
+  assert.strictEqual(sameMap.size(), 5);
 });
 
-test('.toObject', () => {
-    const mapObject = map.toObject();
+test(".toObject", () => {
+  const mapObject = map.toObject();
 
-    assert.strictEqual(mapObject.details.citizen.passportYear, 1999);
-    assert.strictEqual(Object.keys(mapObject).length, 5);
+  assert.strictEqual(mapObject.details.citizen.passportYear, 1999);
+  assert.strictEqual(Object.keys(mapObject).length, 5);
 });
