1006576f99371e8292a15cce4d61dcec43318ea3
Nairi Harutyunyan <nairi.haroutiounian@gmail.com>
2023-09-30 00:57:32 +0300

    Update eslint, add new methods (#10)

--- example/heavy/main.js
+++ example/heavy/main.js
@@ -2,39 +2,38 @@ const { WorkerMap } = require('worker_map');
 const { Worker } = require('worker_threads');
 
 const accountMap = new WorkerMap({
-    owner: 'Elon',
-    wallets_usd_bank: 100,
-    wallets_eth: 0,
+  owner: 'Elon',
+  wallets_usd_bank: 100,
+  wallets_eth: 0,
 });
 
 accountMap.set('wallets_doge', 0);
 accountMap.set('armenian_name', 'Էլոն Մասկ');
 
 setInterval(() => {
-    accountMap.set('wallets_usd_bank', accountMap.get('wallets_usd_bank') - 1);
-    accountMap.set('wallets_eth', accountMap.get('wallets_eth') + 0.6);
-    accountMap.set('wallets_doge', accountMap.get('wallets_doge') + 0.4);
+  accountMap.set('wallets_usd_bank', accountMap.get('wallets_usd_bank') - 1);
+  accountMap.set('wallets_eth', accountMap.get('wallets_eth') + 0.6);
+  accountMap.set('wallets_doge', accountMap.get('wallets_doge') + 0.4);
 }, 50);
 
-
 new Worker('./worker', {
-    workerData: {
-        coin_name: 'doge',
-        sharedAccount: accountMap.toSharedBuffer(),
-    },
+  workerData: {
+    coin_name: 'doge',
+    sharedAccount: accountMap.toSharedBuffer(),
+  },
 });
 
 new Worker('./worker', {
-    workerData: {
-        coin_name: 'eth',
-        sharedAccount: accountMap.toSharedBuffer(),
-    },
+  workerData: {
+    coin_name: 'eth',
+    sharedAccount: accountMap.toSharedBuffer(),
+  },
 });
 
 setInterval(() => {
-    console.log(accountMap.toObject());
+  console.log(accountMap.toObject());
 
-    if (accountMap.get('wallets_usd_bank') <= 1) {
-        process.exit(0);
-    }
+  if (accountMap.get('wallets_usd_bank') <= 1) {
+    process.exit(0);
+  }
 }, 30);
